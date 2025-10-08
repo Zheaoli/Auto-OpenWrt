092059fca42c85f1f6c729a1ea1ed20de2bb9d8e
Nadeshiko Manju <me@manjusaka.me>
2025-10-08 16:47:20 +0800

    Update diy-6.12-part2.sh

--- diy-6.12-part2.sh
+++ diy-6.12-part2.sh
@@ -21,10 +21,8 @@ sed -i 's/192.168.1.1/192.168.40.1/g' package/base-files/files/bin/config_genera
 sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' target/linux/x86/Makefile
 
 ########### 更改默认主题（可选）###########
-# 删除自定义源默认的 argon 主题
-rm -rf package/lean/luci-theme-argon
 # 拉取 argon 原作者的源码
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 # 替换默认主题为 luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 # make menuconfig时记得勾选LuCI ---> Applications ---> luci-app-argon-config
