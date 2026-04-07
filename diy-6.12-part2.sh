e45b9673deb12df0f9870e4003f756a6266c2f51
Manjusaka <me@manjusaka.me>
2026-04-07 16:42:08 +0800

    Upgrade to 6.18 again
    Signed-off-by: Manjusaka <me@manjusaka.me>

--- diy-6.12-part2.sh
+++ diy-6.12-part2.sh
@@ -18,11 +18,13 @@ sed -i 's/192.168.1.1/192.168.40.1/g' package/base-files/files/bin/config_genera
 # sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 ########### 更改大雕源码（可选）###########
-# sed -i 's/KERNEL_PATCHVER:=6.12/KERNEL_PATCHVER:=6.18/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.12/KERNEL_PATCHVER:=6.18/g' target/linux/x86/Makefile
 
 ########### 更改默认主题（可选）###########
+# 删除自定义源默认的 argon 主题
+rm -rf package/lean/luci-theme-argon
 # 拉取 argon 原作者的源码
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 # 替换默认主题为 luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 # make menuconfig时记得勾选LuCI ---> Applications ---> luci-app-argon-config
